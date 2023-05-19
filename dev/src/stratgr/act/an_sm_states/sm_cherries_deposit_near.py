f8be90b402bd37711563d48028e127df38a92564
Hugo <mtnhugo062@gmail.com>
2023-05-19 19:44:04 +0200

    Sans le move nul

--- dev/src/stratgr/act/an_sm_states/sm_cherries_deposit_near.py
+++ dev/src/stratgr/act/an_sm_states/sm_cherries_deposit_near.py
@@ -66,14 +66,14 @@ class ObsDepositCherriesNear(smach.State):
             ## On lance l'action de baisser le bras pour récupérer les cerises et remonter le bras.
             return 'deposit' """
         
-        if userdata.nb_actions_done[0] == 1:
+        """ if userdata.nb_actions_done[0] == 1:
             ## On se déplace jusqu'au site des cerises perpendiculaires au mur (en se mettant dans la bonne orientation pour le bras)
             x, y, z = DEPOSIT_CHERRIES_POS
             y -= ARM_SHIFT
             if userdata.color == 1:
                 z = -z
             set_next_destination(userdata, x, y, z, DISPLACEMENT['noAvoidance'])
-            return 'disp'
+            return 'disp' """
 
         end_of_action_pub.publish(exit=1, reason='success')
         return 'done' 

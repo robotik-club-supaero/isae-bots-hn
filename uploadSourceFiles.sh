24500ca7ceb10e238283a3c89b6710d6c0e7bd16
Antonin Bretagne <emanassociation@gmail.com>
2024-05-09 00:58:51 +0200

    Hot fix: replaced hokuyo with rplidar

--- uploadSourceFiles.sh
+++ uploadSourceFiles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync -av --exclude-from='rsyncExcludeList.txt' dev/src/ pi@robot:isae-bots-hn-2024/dev/src/
+rsync -av dev/src/ pi@192.168.1.11:isae-bots-hn-2024/dev/src/
+rsync -av --exclude-from='rsyncExcludeList.txt' scripts/ pi@192.168.1.11:isae-bots-hn-2024/scripts/

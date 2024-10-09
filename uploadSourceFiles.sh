9f08fac5f8775ab175dbce25dab55efd0e0505ab
Antonin Bretagne <emanassociation@gmail.com>
2024-10-09 19:33:30 +0200

    Impl: updated rsync filters

--- uploadSourceFiles.sh
+++ uploadSourceFiles.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-rsync -av dev/src/ pi@192.168.1.11:isae-bots-hn-2024/dev/src/
-rsync -av --exclude-from='rsyncExcludeList.txt' scripts/ pi@192.168.1.11:isae-bots-hn-2024/scripts/
+rsync -av --delete --filter="merge rsyncFilterList.txt" ./ pi@192.168.1.11:isae-bots-hn-2024/

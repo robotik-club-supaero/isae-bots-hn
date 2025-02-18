f842dbbb110621fa9fac50eefd772507573b1cea
Fearium <letaiseux.muller@gmail.com>
2025-02-18 17:19:04 +0100

    Revert "map defined outside function"
    This reverts commit 0fa3524e370b72373ea756cec5de16cf88356117.

--- dev/src/displacement/displacement/pathfinder/astar_redefined.py
+++ dev/src/displacement/displacement/pathfinder/astar_redefined.py
@@ -35,123 +35,6 @@ import heapq
 from ..disp_utils import GRID_INTERVAL
 from .exceptions import PathNotFoundError, TimeOutError, DestBlockedError
 
-
-#######################################################################
-#
-#                    CLASS USED FOR THE PATHFINDING
-#
-#######################################################################
-
-class Point_astar:
-    def __init__(self, x: float, y: float):
-        self.coords = (x, y)
-        self.children = []
-        self.heuristic = -1
-    
-    def isDifferent(self, point):
-        return not(self.coords[0] == point.getCoords()[0] and self.coords[1] == point.getCoords()[1])
-    
-    def getCoords(self):
-        return self.coords
-    
-    def getChildren(self):
-        return self.children
-
-    def updateChildren(self, map, tableMap):
-        self.eraseChildren()
-        for point in map.getPoints():
-            if can_go_straight(tableMap, self.coords, point.getCoords()) and self.isDifferent(point):
-                self.children.append(Segment_astar(self, point))
-    
-    def eraseChildren(self):
-        self.children = []
-
-    def setHeuristic(self, map):
-        self.heuristic = map.heuristic(self)
-
-    def getHeuristic(self):
-        return self.heuristic
-
-    def addMapData(self, map, tableMap):
-        self.updateChildren(map, tableMap)
-        self.setHeuristic(map)
-
-    def rmvMapData(self):
-        self.eraseChildren()
-        self.heuristic = -1
-
-    def __lt__(self, other):
-        """Less than comparator"""
-        return self.coords < other.coords
-    
-    def __eq__(self, other):
-        """Equality comparator"""
-        return self.coords == other.coords
-    
-    def __gt__(self, other):
-        """Greater than comparator"""
-        return self.coords > other.coords
-    
-    def __le__(self, other):
-        """Less than or equal comparator"""
-        return self.coords <= other.coords
-    
-    def __ge__(self, other):
-        """Greater than or equal comparator"""
-        return self.coords >= other.coords
-
-
-class Segment_astar:
-    def __init__(self, init: Point_astar, dest: Point_astar):
-        self.init   = init
-        self.dest   = dest
-        self.length = self.calculateLength()
-
-    def getInit(self):
-        return self.init
-    
-    def getDest(self):
-        return self.dest
-    
-    def getLength(self):
-        return self.length
-
-    def calculateLength(self):
-        pos = self.init.getCoords()
-        obj = self.dest.getCoords()
-        return np.sqrt((pos[0]-obj[0])**2 + (pos[1]-obj[1])**2)
-    
-
-class Map_astar:
-    def __init__(self, init: Point_astar, dest: Point_astar):
-        self.points = [init, dest]
-        self.goal  = dest
-        self.start  = init
-
-    def addPoint(self, point: Point_astar):
-        self.points.append(point)
-
-    def getPoints(self):
-        return self.points
-    
-    def getStart(self):
-        return self.start
-    
-    def getGoal(self):
-        return self.goal
-    
-    def heuristic(self, point: Point_astar):
-        goal = self.goal.coords
-        pos  = point.coords
-        return np.sqrt((pos[0]-goal[0])**2 + (pos[1]-goal[1])**2)
-    
-    def updateMap(self, tableMap):
-        for point in self.points:
-            Point_astar.addMapData(self=point, map=self, tableMap=tableMap)
-
-
-
-
 ### CONSTANTES ########################################################
 
 #######################################################################
@@ -200,27 +83,22 @@ def a_star(init, goal, tableMap, weights, _maxAstarTime, logger):
     start = [min(int(init[0]), GRID_INTERVAL*(weights.shape[0]-1)), min(int(init[1]), GRID_INTERVAL*(weights.shape[1]-1))]
     dest = [min(int(goal[0]), GRID_INTERVAL*(weights.shape[0]-1)), min(int(goal[1]), GRID_INTERVAL*(weights.shape[1]-1))]
 
-
-    astarMap = Map_astar(Point_astar(start[0], start[1]), Point_astar(dest[0], dest[1]))
-    
+    astarMap = [dest]
     obstacles = tableMap.get_obstacles()
 
     # on crée la map avant d'exécuter l'algo
     for obstacle in obstacles:
         corners = obstacle.corners()
         for corner in corners:
-            astarMap.addPoint(Point_astar(corner[0], corner[1]))
-    
-    astarMap.updateMap(tableMap)  # Toujours bien penser a update la map après construction pour ajouter les connexions  
-
+            astarMap.append(corner)
 
     pre_process_time = time.perf_counter()
+   
+    # Grid reducer: idea --> remove from grid the positions corresponding to empty zones and obstacles, only keep borders of obstacles
 
+    path = astar_path(tableMap, astarMap, start, dest, _maxAstarTime) # see implementation at the end --> objective is to implement such method in C++
 
-    #path = astar_path(tableMap, astarMap, start, dest, _maxAstarTime) # see implementation at the end --> objective is to implement such method in C++
-    path = astar_path_v2(tableMap, astarMap, _maxAstarTime)
-
-    # print("end of a_star algorithm \n")
+    #print("end of a_star algorithm \n")
     
     if path == -1:
         raise TimeOutError
@@ -261,17 +139,15 @@ def a_star(init, goal, tableMap, weights, _maxAstarTime, logger):
 #
 #######################################################################
 
-def astar_path_v2(tableMap, map: Map_astar, strMaxTime):
-    init_time = time.perf_counter()
+def astar_path(tableMap, astarMap, start, goal, strMaxTime):
 
-    start = map.getStart()
-    goal  = map.getGoal()
+    init_time = time.perf_counter()
 
     visited = []
     openQ = []
-    start_state = (0, start, [start.getCoords()], 0)
-    # le state contient dans l'ordre (total, Point_astar, path = [(float, float)], coût)
-    # heapq trie les éléments en fonction de la première variable, donc total, ainsi la priorité est donné au path avec le coût total le plus faible
+    start_state = (0, start, [start], 0) 
+    # le state contient dans l'ordre (total, position, path, coût)
+    # heapq trie les éléments en fonction de la première variable, donc total
     heapq.heappush(openQ, start_state)
 
     while len(openQ) >0:
@@ -280,23 +156,41 @@ def astar_path_v2(tableMap, map: Map_astar, strMaxTime):
             return -1
 
         s = heapq.heappop(openQ)
-        pos = s[1]
-        visited.append(pos.getCoords())
+        visited.append(s[1])
 
-        if not(pos.isDifferent(goal)):
+        if s[1][0] == goal[0] and s[1][1] == goal[1]:
+            # on a trouvé le path otpimal vers l'objectif
             path = s[2].copy()
             return path
-        
-        for vectNext in pos.getChildren():
-            nextPos = vectNext.getDest()
-            if nextPos.getCoords() in visited:
+
+        for point in astarMap:
+            if point in visited:
                 continue
             else:
-                cost = vectNext.getLength() + s[3]
-                g    = cost + nextPos.getHeuristic()
+                if can_go_straight(tableMap, s[1], point):
+                        c = cost(s[1], point) + s[3] # calcul du nouveau coût
+                        g = c + heuristic(point, goal) # calcul de la fontcion obj totale (coût + heuristic)
 
-                tpath = s[2].copy()
-                tpath.append(nextPos.getCoords())
+                        tpath = s[2].copy()
+                        tpath.append(point)
 
-                nextS = (g, nextPos, tpath, cost)
-                heapq.heappush(openQ, nextS)
+                        ns = (g, point, tpath, c)
+                        heapq.heappush(openQ, ns)
+
+    return None
+
+
+def cost(pos, obj):
+
+    dist = np.sqrt((pos[0]-obj[0])**2 + (pos[1]-obj[1])**2)
+    return dist
+
+def heuristic(pos, goal):
+    '''
+    Implémentation de l'heuristique àl'aide de la norme 2, qui domine la
+    norme infinie (ie meilleur indicateur pour le pathfinding), et reste
+    admissible (ie: ne surestime pas la distance réelle)
+    '''
+
+    dist = np.sqrt((pos[0]-goal[0])**2 + (pos[1]-goal[1])**2)
+    return dist

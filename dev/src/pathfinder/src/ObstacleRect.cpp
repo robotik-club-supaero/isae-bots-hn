2063994dae6a323c2dfb658bb34b0d938bbf5160
Bretagne Antonin <bretagne.antonin@free.fr>
2025-04-26 23:28:24 +0200

    Fix: bug in pathfinder (collision with wall not detected)

--- dev/src/pathfinder/src/ObstacleRect.cpp
+++ dev/src/pathfinder/src/ObstacleRect.cpp
@@ -18,35 +18,42 @@ std::vector<Point> ObstacleRect::getCorners() const {
     return {m_topLeft, m_topLeft + Point(getWidth(), 0), m_bottomRight, m_topLeft + Point(0, getHeight())};
 }
 
-bool in_range(double value, double min, double max) { return min <= value && value <= max; }
+bool line_line_intersect(const Segment &line1, const Segment &line2) {
+    Point b = line1.end - line1.start;
+    Point d = line2.end - line2.start;
+    float bDotDPerp = b.getX() * d.getY() - b.getY() * d.getX();
+
+    // if b dot d == 0, it means the lines are parallel so have infinite intersection points
+    if (bDotDPerp == 0) {
+        return false;
+    }
+
+    Point c = line2.start - line1.start;
+    float t = (c.getX() * d.getY() - c.getY() * d.getX()) / bDotDPerp;
+    if (t < 0 || t > 1) {
+        return false;
+    }
+
+    float u = (c.getX() * b.getY() - c.getY() * b.getX()) / bDotDPerp;
+    if (u < 0 || u > 1) {
+        return false;
+    }
+
+    return true;
+}
 
 bool ObstacleRect::crosses(Segment segment) const {
-    double xMin = m_topLeft.getX();
-    double xMax = m_bottomRight.getX();
-    double yMin = m_topLeft.getY();
-    double yMax = m_bottomRight.getY();
-
-    double originX = segment.start.getX();
-    double originY = segment.start.getY();
-
-    Point lineVect = segment.getDirection();
-    if (lineVect.getX() != 0) {
-        double tMin = (xMin - originX) / lineVect.getX();
-        double tMax = (xMax - originX) / lineVect.getX();
-
-        if ((in_range(originY + tMin * lineVect.getY(), yMin, yMax) && in_range(tMin, 0, 1)) ||
-            (in_range(originY + tMax * lineVect.getY(), yMin, yMax) && in_range(tMax, 0, 1))) {
-            return true;
-        }
+    if (line_line_intersect(segment, Segment(m_topLeft, Point(m_bottomRight.getX(), m_topLeft.getY())))) {
+        return true;
     }
-    if (lineVect.getY() != 0) {
-        double tMin = (yMin - originY) / lineVect.getY();
-        double tMax = (yMax - originY) / lineVect.getY();
-
-        if ((in_range(originX + tMin * lineVect.getX(), xMin, xMax) && in_range(tMin, 0, 1)) ||
-            (in_range(originX + tMax * lineVect.getX(), xMin, xMax) && in_range(tMax, 0, 1))) {
-            return true;
-        }
+    if (line_line_intersect(segment, Segment(m_topLeft, Point(m_topLeft.getX(), m_bottomRight.getY())))) {
+        return true;
+    }
+    if (line_line_intersect(segment, Segment(Point(m_bottomRight.getX(), m_topLeft.getY()), m_bottomRight))) {
+        return true;
+    }
+    if (line_line_intersect(segment, Segment(Point(m_topLeft.getX(), m_bottomRight.getY()), m_bottomRight))) {
+        return true;
     }
     return false;
 }

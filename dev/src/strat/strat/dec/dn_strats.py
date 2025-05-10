5e00cc68fb15604c5c0b740656c4592ae8397c31
Arthur1459 <arthur.oudeyer@icloud.com>
2025-05-10 13:19:55 +0200

    Strat Updated

--- dev/src/strat/strat/dec/dn_strats.py
+++ dev/src/strat/strat/dec/dn_strats.py
@@ -122,11 +122,11 @@ def match_strat(node):
             if stand_id is not None:
                 if not node.action_successful: # Continue to search for stand                
                     node.curr_action[1] = stand_id
-                    node.get_logger().info(f"Next action order : Picking up Stand n°{stand_id}...")        
+                    node.get_logger().info(f"Continue action order : Picking up Stand n°{stand_id}...")        
                 elif node.curr_action[0] == Action.PICKUP_STAND_2: # top stand picked -> Go bottom stand picking
                     node.remaining_stands[node.curr_action[1]] = 0
                     node.curr_action = [Action.PICKUP_STAND_1, stand_id]
-                    node.get_logger().info(f"Next action order : Pickup Stand n°{stand_id}")
+                    node.get_logger().info(f"Next action order : STAND_1 -> Pickup Stand n°{stand_id}")
                 elif node.curr_action[0] == Action.PICKUP_STAND_1: # bottom stand picked -> Go deposit
                     node.remaining_stands[node.curr_action[1]] = 0
                     deposit_id = find_closest(node, DEPOSIT_POS, node.deposit_slots, coeffs=malus_deposit, pos_type='deposit')
@@ -155,10 +155,11 @@ def match_strat(node):
                     node.get_logger().info("No more free slot to deposit !")
 
         # If no other action is applicable, defaulting to picking up stand
-        node.get_logger().warning("Out of Normal Loop : switching to Pickup Stand 2")
+        node.get_logger().info("Out of Normal Loop : switching to Pickup Stand 2")
         stand_id = find_closest(node, STAND_POS, node.remaining_stands, coeffs=malus_pickup, pos_type='stand')
         if stand_id is not None:
             node.curr_action = [Action.PICKUP_STAND_2, stand_id]
+            node.get_logger().info(f"Next action order : STAND_2 -> Pickup Stand n°{stand_id}")
             node.publishAction()
             return
         else:

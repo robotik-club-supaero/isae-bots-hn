0d42e3caa1c39bee6887317c7056bdf82b435560
Thomas <robergethomas@orange.fr>
2026-01-13 22:57:28 +0100

    Début du code de positionnement par Lidar

---
+++ dev/src/sensors/sensors/lidar/lidar_positioning_config.py
@@ -0,0 +1,33 @@
+import math
+
+TABLE_H = 3000          # hauteur de table (selon y)
+TABLE_W = 2000          # largeur de table (selon x)
+
+# Offset with respect to the center of the robot (if the lidar is not centered)
+LIDAR_OFFSET_X = 0
+LIDAR_OFFSET_Y = 0
+
+# Max distance between two *successive* measurements for them to belong to the same cluster
+CLUSTER_DIST_LIM = 100 # mm
+
+# Ignore detections closer than this
+MIN_RANGE = 50 # mm
+
+# Ignore detections further than this
+MAX_RANGE = 3000 # mm
+
+# Number of points in a cluster for it to be valid
+DETECTION_THRESHOLD = 10
+
+# Coordinates of the three marks
+MARK_1_X = 1
+MARK_1_Y = 1
+
+MARK_2_X = 1
+MARK_2_Y = 1
+
+MARK_3_X = 1
+MARK_3_Y = 1
+
+# Uncertainty on the distance between the marks
+EPS = 10 #mm
